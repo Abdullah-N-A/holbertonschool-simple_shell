64d9a17374681ce8568dbab312ab8dc27d978024
Fahd36631 <Fahd36631@gmail.com>
2025-11-09 20:10:02 +0300

    Add header file for shell functionality

--- shell.h
+++ shell.h
@@ -1,1 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+extern char **environ;
+
+#define PROMPT "#cisfun$ "
+
+int run_shell(char *progname);
+
+#endif /* SHELL_H */
