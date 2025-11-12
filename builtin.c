718bedaa138820b4b68fa8d627d2ee07be1989d4
Fahd36631 <fahd36631@gmail.com>
2025-11-12 15:22:27 +0000

    Implement built-in commands (exit and env) with proper error handling

--- builtin.c
+++ builtin.c
@@ -1,28 +1,72 @@
 #include "shell.h"
 
-extern char **environ;  /* Add this line */
-
-/* Exit built-in */
+/**
+ * builtin_exit - handles exit command
+ * @args: command arguments
+ *
+ * Return: -1 to signal program exit
+ */
 int builtin_exit(char **args)
 {
-    if (strcmp(args[0], "exit") == 0)
-        return 1;
-    return 0;
+	int exit_code = 0;
+
+	if (args[1])
+	{
+		if (!validate_number(args[1]))
+		{
+			fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", args[1]);
+			return (0);
+		}
+
+		exit_code = str_to_int(args[1]);
+		exit_code = exit_code % 256;
+		if (exit_code < 0)
+			exit_code = 256 + exit_code;
+	}
+
+	exit(exit_code);
 }
 
-/* Env built-in */
+/**
+ * builtin_env - prints all environment variables
+ * @args: command arguments (unused)
+ *
+ * Return: 1 after execution
+ */
 int builtin_env(char **args)
 {
-    int i;
+	int i;
 
-    if (strcmp(args[0], "env") == 0)
-    {
-        for (i = 0; environ[i]; i++)
-        {
-            write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-            write(STDOUT_FILENO, "\n", 1);
-        }
-        return 1;
-    }
-    return 0;
+	(void)args;
+
+	if (!environ)
+		return (1);
+
+	for (i = 0; environ[i]; i++)
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+
+	return (1);
+}
+
+/**
+ * check_builtin - determines if command is built-in
+ * @args: command arguments
+ *
+ * Return: -1 for exit, 1 if builtin executed, 0 if not builtin
+ */
+int check_builtin(char **args)
+{
+	if (!args || !args[0])
+		return (0);
+
+	if (strcmp(args[0], "exit") == 0)
+		return (builtin_exit(args));
+
+	if (strcmp(args[0], "env") == 0)
+		return (builtin_env(args));
+
+	return (0);
 }
